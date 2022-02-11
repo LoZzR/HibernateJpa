5b6bdd14d8f26b0f8ef5dc3309a0cfc8bb021e77
LoZzR <zakariaelhichem@gmail.com>
2022-02-11 18:43:49 +0100

    Add persistence unit definition: persistence.xml file

---
+++ src/main/java/com/entities/Message.java
@@ -0,0 +1,19 @@
+package com.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String text;
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+}
