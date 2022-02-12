e3af6f0186a72e12a12ae0e79cbb691f2c21cc15
LoZzR <zakariaelhichem@gmail.com>
2022-02-12 20:10:08 +0100

    Add DataBase Connexion

---
+++ src/main/java/com/entities/HelloJpa.java
@@ -0,0 +1,10 @@
+package com.entities;
+
+public class HelloJpa {
+    /*private static final EntityManagerFactory emFactoryObj;
+    private static final String PERSISTENCE_UNIT_NAME = "JPADemo";
+
+    static {
+        emFactoryObj = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    }*/
+}
