c3e447d0e91a6f4604da7b1663027e85c54b36d8
LoZzR <zakariaelhichem@gmail.com>
2022-02-20 23:55:41 +0100

    Elaborate first example queries

--- src/main/java/com/HelloJpa.java
+++ src/main/java/com/HelloJpa.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.transaction.UserTransaction;
+import java.util.List;
 
 public class HelloJpa {
     private static final EntityManagerFactory emFactoryObj;
@@ -18,10 +19,17 @@ public class HelloJpa {
     public static void main(String args[]){
         EntityManager em = emFactoryObj.createEntityManager();
         em.getTransaction().begin();
-        Message message = new Message();
+        /*Message message = new Message();
         message.setId(Long.valueOf(1));
         message.setText("Hello World!");
-        em.persist(message);
+        em.persist(message);*/
+
+        List<Message> messages = em.createQuery("select m from Message m").getResultList();
+
+        System.out.println(messages);
+
+        messages.get(0).setText("Take me to your leader!");
+
         em.getTransaction().commit();
         // INSERT into MESSAGE (ID, TEXT) values (1, 'Hello World!')
         em.close();
