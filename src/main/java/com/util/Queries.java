89d87769bcf1d8944ab7a2908756b791657879c2
Zakariae <zakariaelhichem@gmail.com>
2022-03-24 21:46:34 +0100

    Add dynamic + named queries

---
+++ src/main/java/com/util/Queries.java
@@ -0,0 +1,39 @@
+package com.util;
+
+import com.entities.Item;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@NamedQueries({
+        @NamedQuery(
+                name = "findItemsOrderByName",
+                query = "select i from Item i order by i.name asc"
+        )
+        ,
+        @NamedQuery(
+                name = "findItemBuyNowPriceGreaterThan",
+                query = "select i from Item i where i.buyNowPrice > :price"
+        )
+})
+public class Queries {
+
+    public List<Item> getDynamicItems() {
+        EntityManager em = EntityManagerHelper.getEntityManager();
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Item> query =
+                cb.createQuery(Item.class);
+        Root<Item> fromItem = query.from(Item.class);
+        query.select(fromItem);
+        List<Item> items = em.createQuery(query).getResultList();
+
+        return items;
+    }
+
+}
