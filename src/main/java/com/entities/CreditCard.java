1380978dfa60de0ac4a62e636a9596eb8069fee6
Zakariae <zakariaelhichem@gmail.com>
2022-04-14 23:55:25 +0000

    Add BillingDetails class and it subclasses using table per concrete class strategy

---
+++ src/main/java/com/entities/CreditCard.java
@@ -0,0 +1,68 @@
+package com.entities;
+
+import com.util.Constants;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@AttributeOverride(
+        name = "owner",
+        column = @Column(name = "CC_OWNER", nullable = false))
+public class CreditCard extends BillingDetails {
+
+    @Id
+    @GeneratedValue(generator = Constants.ID_GENERATOR)
+    protected Long id;
+
+    @NotNull
+    protected String cardNumber;
+
+    @NotNull
+    protected String expMonth;
+
+    @NotNull
+    protected String expYear;
+
+    // ...
+
+    public CreditCard() {
+        super();
+    }
+
+    public CreditCard(String owner, String cardNumber, String expMonth, String expYear) {
+        super(owner);
+        this.cardNumber = cardNumber;
+        this.expMonth = expMonth;
+        this.expYear = expYear;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpMonth() {
+        return expMonth;
+    }
+
+    public void setExpMonth(String expMonth) {
+        this.expMonth = expMonth;
+    }
+
+    public String getExpYear() {
+        return expYear;
+    }
+
+    public void setExpYear(String expYear) {
+        this.expYear = expYear;
+    }
+
+}
