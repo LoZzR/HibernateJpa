b4f6f78d8544a1f2ee27759f99f4bcaf729afa49
LoZzR <zakariaelhichem@gmail.com>
2022-04-16 23:56:17 +0000

    Use InheritanceType.TABLE_PER_CLASS strategy

--- src/main/java/com/entities/BillingDetails.java
+++ src/main/java/com/entities/BillingDetails.java
@@ -2,7 +2,12 @@ package com.entities;
 
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
+import javax.persistence.Enity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
+/*@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)*/
 @MappedSuperclass
 public abstract class BillingDetails {
 
